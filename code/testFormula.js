d8c93160e2a01324c23ea6b5f2582b8765f0efbb
Océane Patiny <oceane@patiny.com>
2017-01-16 17:39:02 +0100

    modifing formula code

--- code/testFormula.js
+++ code/testFormula.js
@@ -1,6 +1,9 @@
 "use srict";
 
-var formula = require('./formula.js');
+var formula1 = require('./formula1.js');
+var formula2 = require('./formula2.js');
+var formula3 = require('./formula3.js');
+var formula4 = require('./formula4.js');
 
 var radiusCenter = 40; // rayon du cercle défini par la masse au centre du cylindre en [mm]
 var radiusServo = 40; // rayon défini par l'axe du servo en [mm]
@@ -12,7 +15,10 @@ var results=[];
 
 
 for( var angleCenter = 0; angleCenter <= 180; angleCenter+=10) {
-    var angleServo1 =  formula(radius, radiusServo, radiusCenter, angleCenter, distance);
+    var angleServo11 =  formula1(radius, radiusServo, radiusCenter, angleCenter, distance);
+    var angleServo12 =  formula2(radius, radiusServo, radiusCenter, angleCenter, distance);
+    var angleServo13 =  formula3(radius, radiusServo, radiusCenter, angleCenter, distance);
+    var angleServo14 =  formula4(radius, radiusServo, radiusCenter, angleCenter, distance);
  //   var angleServo2 =  formula(radius, radiusServo, radiusCenter, (angleCenter+120), distance);
  //   var angleServo3 =  formula(radius, radiusServo, radiusCenter, (angleCenter+240), distance);
     
@@ -21,10 +27,13 @@ for( var angleCenter = 0; angleCenter <= 180; angleCenter+=10) {
         radiusServo,
         radius,
         distance,
-        angleServo1,
+        angleServo11,
+        angleServo12,
+        angleServo13,
+        angleServo14,
         angleCenter
     });
-    console.log(angleCenter+'\t'+angleServo1.a1);
+    console.log(angleCenter+'\t'+angleServo11.a1+'\t'+angleServo12.a1+'\t'+angleServo13.a1+'\t'+angleServo14.a1);
 }
 
 //console.log(results);
